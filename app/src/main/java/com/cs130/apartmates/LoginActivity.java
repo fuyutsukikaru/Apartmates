6df09ca2c1fe0ee821df89709512f4dccc067088
Soomin Jeong <sjeongus@gmail.com>
2015-10-17 02:03:55 -0700

    Added a login page with redirect to signup

--- app/src/main/java/com/cs130/apartmates/LoginActivity.java
+++ app/src/main/java/com/cs130/apartmates/LoginActivity.java
@@ -1,15 +1,19 @@
 package com.cs130.apartmates;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CUSTOM_TABS_SESSION =
+            "android.support.customtabs.extra.SESSION";
+    private static final String EXTRA_CUSTOM_TABS_EXIT_ANIMATION_BUNDLE =
+            "android.support.customtabs.extra.EXIT_ANIMATION_BUNDLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +22,16 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setTitle(getString(R.string.login_activity));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+    public void onSignUpClick(View v) {
+        String url = "https://venmo.com/signup";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        Bundle extras = new Bundle();
+        extras.putBinder(EXTRA_CUSTOM_TABS_SESSION, null);
+        intent.putExtras(extras);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        startActivity(intent);
     }
 }
