3f866d29b084e8c1e1669a97632132412bd40b16
Brett Chalabian <brettchalabian@gmail.com>
2015-10-27 11:40:37 -0700

    Fixed bounty task layout to include action bar.

--- app/src/main/java/com/cs130/apartmates/activities/BountyActivity.java
+++ app/src/main/java/com/cs130/apartmates/activities/BountyActivity.java
@@ -2,8 +2,10 @@ package com.cs130.apartmates.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.cs130.apartmates.R;
 
@@ -13,7 +15,7 @@ import java.util.Vector;
 /**
  * Created by bchalabian on 10/26/15.
  */
-public class BountyActivity extends Activity {
+public class BountyActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private BTAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -25,7 +27,9 @@ public class BountyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.bounty_layout);
+        setContentView(R.layout.activity_tasks);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.rv);
         mRecyclerView.setHasFixedSize(true);
 
