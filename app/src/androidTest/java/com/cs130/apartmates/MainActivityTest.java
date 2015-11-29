1d7dd845a1af2dec24c15b3c32cfb86796374691
Eric Du <ericvdu@ucla.edu>
2015-11-28 20:41:12 -0800

    Group leaving, joining

--- app/src/androidTest/java/com/cs130/apartmates/MainActivityTest.java
+++ app/src/androidTest/java/com/cs130/apartmates/MainActivityTest.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
 import com.cs130.apartmates.activities.MainActivity;
 import com.robotium.solo.Solo;
 
@@ -95,7 +97,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             assertTrue(solo.searchText("Cancel")); //Search for cancel button text
             Thread.sleep(500);
         } catch (Exception e) {
-
+            System.err.print("An error has occurred.");
         }
         solo.finishOpenedActivities();
     }
@@ -114,7 +116,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             assertTrue(solo.searchText("Cancel")); //Search for cancel button text
             Thread.sleep(500);
         } catch (Exception e) {
-
+            System.err.print("An error has occurred.");
         }
         solo.finishOpenedActivities();
     }
@@ -129,7 +131,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             assertTrue(solo.searchText("Leave Group")); //Should find a leave group button
             Thread.sleep(500);
         } catch (Exception e) {
-
+            System.err.print("An error has occurred.");
         }
         solo.finishOpenedActivities();
     }
@@ -151,8 +153,112 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             assertTrue(solo.searchText("Cancel")); //Search for cancel button text
             Thread.sleep(1000);
         } catch (Exception e) {
-
+            System.err.print("An error has occurred.");
         }
+        solo.finishOpenedActivities();
+    }
+
+    public void testAddRemoveRotationTask() {
+        try {
+            solo.sendKey(Solo.ENTER);
+
+            solo.clickOnImageButton(0);
+
+            //Not sure why this statement takes a while to execute
+            solo.clickOnMenuItem("Rotation Tasks");
+            assertTrue(solo.searchText("Rotation Tasks"));
+            solo.clickOnImageButton(1);
+            assertTrue(solo.waitForActivity("AddRoTaskActivity", 5000));
+
+            //Fill in fields and create task
+            EditText mTitle = solo.getEditText(0);
+            EditText mDescription = solo.getEditText(1);
+            EditText mValue = solo.getEditText(2);
+            EditText mDeadline = solo.getEditText(3);
+            solo.enterText(mTitle, "Test Rotation Task");
+            solo.enterText(mDescription, "Test description");
+            solo.enterText(mValue, "5");
+            solo.enterText(mDeadline, "3");
+            solo.clickOnButton(0);
+            assertTrue(solo.waitForActivity("MainActivity", 5000));
+
+            solo.clickOnText("Test Rotation Task");
+            assertTrue((solo.searchText("Delete", true)));
+            assertTrue((solo.searchText("Delete", true)));
+            solo.clickOnButton("Delete");
+            assertFalse((solo.searchText("Test Rotation Task")));
+
+            Thread.sleep(10000);
+        }
+        catch(Exception e) {
+            System.err.print("An error has occurred.");
+        }
+    }
+
+    public void testAddRemoveBountyTask() {
+        try {
+            solo.sendKey(Solo.ENTER);
+
+            solo.clickOnImageButton(0);
+
+            //Not sure why this statement takes a while to execute
+            solo.clickOnMenuItem("Bounty Tasks");
+            assertTrue(solo.searchText("Bounty Tasks"));
+            solo.clickOnImageButton(1);
+            assertTrue(solo.waitForActivity("AddTaskActivity", 5000));
+
+            //Fill in fields and create task
+            EditText mTitle = solo.getEditText(0);
+            EditText mDescription = solo.getEditText(1);
+            EditText mValue = solo.getEditText(2);
+            EditText mDeadline = solo.getEditText(3);
+            solo.enterText(mTitle, "Test Bounty Task");
+            solo.enterText(mDescription, "Test description");
+            solo.enterText(mValue, "5");
+            solo.enterText(mDeadline, "2020-11-29");
+            solo.clickOnButton(0);
+            assertTrue(solo.waitForActivity("MainActivity", 5000));
+
+            //Search for added bounty task, then drop
+            solo.clickOnText("Test Bounty Task");
+            assertTrue((solo.searchText("Drop", true)));
+            solo.clickOnButton("Drop");
+            assertFalse((solo.searchText("Test Bounty Task")));
+
+            Thread.sleep(1000);
+        }
+        catch(Exception e) {
+            System.err.print("An error has occurred.");
+        }
+    }
+
+    //Group ID 18, password is 'test'
+    public void testLeaveJoinGroup() {
+        try {
+            solo.sendKey(Solo.ENTER);
+
+            //Leave the group by navigating to drawer -> Group Info -> Leave Group
+            solo.clickOnImageButton(0);
+            solo.clickOnMenuItem("Group Info");
+            solo.waitForActivity("GroupInfoActivity", 5000);
+            assertTrue(solo.searchText("Leave Group")); //Should find a leave group button
+
+            solo.clickOnButton("Leave Group");
+            solo.waitForActivity("GroupCreateActivity", 5000);
+
+            //Rejoin the group by entering the group ID and password
+            assertTrue(solo.searchText("Join Group"));
+            assertTrue(solo.searchText("Create Group"));
+            solo.enterText(solo.getEditText(0), "18");
+            solo.enterText(solo.getEditText(1), "test");
+            solo.clickOnButton("Join Group");
+
+            solo.waitForActivity("MainActivity", 5000);
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.err.print("An error has occurred.");
+        }
+
         solo.finishOpenedActivities();
     }
 }
